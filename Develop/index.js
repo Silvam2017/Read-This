2e857c9b126d4be6752a70f9ea65973ffd813a51
Michael Silva <msilva@aggienetwork.com>
2020-05-17 15:16:53 -0500

    installed inquirer and node. added code from module 9, as it is similar to our challenge's task

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,157 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+const generatePage = require('.template-page');
 // array of questions for user
-const questions = [
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub Username',
+          validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub Username.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'about',
+          message: 'Provide some information about yourself:'
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAbout',
+          message: 'Would you like to enter some information about yourself for an "About" section?',
+          default: true
+        },
+        {
+          type: 'input',
+          name: 'about',
+          message: 'Provide some information about yourself:',
+          when: ({ confirmAbout }) => confirmAbout
+        }
+      ]);
+    };
+;
+const promptProject = portfolioData => {
 
-];
+    if (!portfolioData.projects) {
+      portfolioData.projects = [];
+    }
+    
+      console.log(`
+      =================
+      Add A New Project
+      =================
+      `);
+      return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of your project?',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter the name of your project.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project (Required)',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('Please enter a project description.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you make this project with? (Check all that apply)',
+          choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+          type: 'input',
+          name: 'link',
+          message: 'Enter the GitHub link to your project (Required)',
+          validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('Please enter the link to your GitHub Repository.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'feature',
+          message: 'Would you like to feature this project?',
+          default: false
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAddProject',
+          message: 'Would you like to enter another project?',
+          default: false
+        }
+      ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+    };
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 
 // function to initialize program
-function init() {
-
-}
+// function init() {}
 
 // function call to initialize program
-init();
+// init();
